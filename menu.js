a9d687810fc32a8d709841e3711dfd4a14f593a2
Сергей Гультяев < >
2017-01-03 22:18:04 +0200

    Сделал меню

--- menu.js
+++ menu.js
@@ -10,6 +10,22 @@ function scroll() {
 window.onscroll = scroll;
 
 
+var btnMenu = document.getElementById('menu'),
+		nav = document.querySelector('.nav');
+
+btnMenu.onclick = function() {
+	nav.style.display = "flex";
+}
+
+window.onclick = function(event) {
+	if (event.target == nav) {
+		nav.style.display = "none";
+	}
+}
+
+
+//Скролл. Просто так
+
 function scrollMe() {	
 		var scroll = setInterval(function() {
 			window.scrollTo(0, window.pageYOffset - 6);
