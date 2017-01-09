7930b28302f1d01fd3cf99d19c69941fe0fc4890
Сергей Гультяев < >
2017-01-09 17:59:27 +0200

    Blur added

--- menu.js
+++ menu.js
@@ -1,3 +1,4 @@
+//Сдвиг меню при скролле
 function scroll() {
 	var y = window.pageYOffset;
 	if (y > 400) {
@@ -9,17 +10,23 @@ function scroll() {
 
 window.onscroll = scroll;
 
-
+//Меню сайта
 var btnMenu = document.getElementById('menu'),
 		nav = document.querySelector('.nav');
 
 btnMenu.onclick = function() {
 	nav.style.display = "flex";
+	nav.style.opacity = 1;
+	document.querySelector('.wrapper').style.filter = "blur(5px)";
+	document.querySelector('.logo-link').style.filter = "blur(5px)";
 }
 
 window.onclick = function(event) {
 	if (event.target == nav) {
 		nav.style.display = "none";
+		nav.style.opacity = 0;
+		document.querySelector('.wrapper').style.filter = "none";
+		document.querySelector('.logo-link').style.filter = "none";
 	}
 }
 
